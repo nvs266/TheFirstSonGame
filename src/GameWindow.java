3fbb5cf0fdaab1a82861bc39b7f09bcf540da23d
cuonghx2709 <cuonghx2709@gmail.com>
2017-07-31 15:59:21 +0700

    added Gamewindow

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,24 @@
+import bases.Setting;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * Created by cuonghx2709 on 7/31/2017.
  */
-public class GameWindow {
+public class GameWindow extends JFrame implements Setting{
+    public GameWindow(){
+        this.setSize(widthScreen, heightScreen);
+        this.setTitle("the first son");
+        this.setResizable(false);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
 }
