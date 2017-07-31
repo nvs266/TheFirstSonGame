efbd9966a3c689db93520615431f25f66fbb2204
cuonghx2709 <cuonghx2709@gmail.com>
2017-07-31 16:35:09 +0700

    Test

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * Created by cuonghx2709 on 7/31/2017.
  */
-public class GameWindow {
+public class GameWindow extends JFrame{
+    public GameWindow(){
+        this.setResizable(false);
+        this.setSize(600,600);
+        this.setTitle("the first son");
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        this.setVisible(true);
+    }
 }
