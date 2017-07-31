8fa15ea35550f028b2be3b604790c0b3a05f09c7
Son Nguyen <nvs266@gmail.com>
2017-07-31 21:47:26 +0700

    Chinh frame ra giua

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -9,6 +9,7 @@ public class GameWindow extends JFrame{
     public GameWindow(){
         this.setResizable(false);
         this.setSize(600,600);
+        this.setLocation(400, 50);
         this.setTitle("the first son");
         this.addWindowListener(new WindowAdapter() {
             @Override
