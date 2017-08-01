44339b916d3903b3eeecb528098a98f2039b13e2
Son Nguyen <nvs266@gmail.com>
2017-08-01 13:02:42 +0700

    Added InputManager

--- src/inputs/InputManager.java
+++ src/inputs/InputManager.java
@@ -1,4 +1,62 @@
 package inputs;
 
+import java.awt.event.KeyEvent;
+
 public class InputManager {
+    public boolean upPressed;
+    public boolean downPressed;
+    public boolean leftPressed;
+    public boolean rightPressed;
+    public boolean spacePressed;
+    public boolean enterPressed;
+
+    public void keyPressed(KeyEvent keyEvent) {
+        switch(keyEvent.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                upPressed = true;
+                break;
+            case KeyEvent.VK_DOWN:
+                downPressed = true;
+                break;
+            case KeyEvent.VK_LEFT:
+                leftPressed = true;
+                break;
+            case KeyEvent.VK_RIGHT:
+                rightPressed = true;
+                break;
+            case KeyEvent.VK_SPACE:
+                spacePressed = true;
+                break;
+            case KeyEvent.VK_ENTER:
+                enterPressed = true;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void keyRealeased(KeyEvent keyEvent) {
+        switch(keyEvent.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                upPressed = false;
+                break;
+            case KeyEvent.VK_DOWN:
+                downPressed = false;
+                break;
+            case KeyEvent.VK_LEFT:
+                leftPressed = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                rightPressed = false;
+                break;
+            case KeyEvent.VK_SPACE:
+                spacePressed = false;
+                break;
+            case KeyEvent.VK_ENTER:
+                enterPressed = false;
+                break;
+            default:
+                break;
+        }
+    }
 }
