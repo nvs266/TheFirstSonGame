925ef63ff11080494dfb527a699d3982929cb80b
cuonghx2709 <cuonghx2709@gmail.com>
2017-08-01 14:19:41 +0700

    added player

--- src/players/Player.java
+++ src/players/Player.java
@@ -1,4 +1,79 @@
 package players;
 
-public class Player {
+import Utils.Utils;
+import bases.FrameCounter;
+import bases.GameObject;
+import bases.Vector2D;
+import bases.renderers.ImageRenderer;
+import inputs.InputManager;
+
+public class Player extends GameObject{
+
+    private static Player instence;
+
+    InputManager inputManager;
+    Vector2D velocity;
+    Vector2D vUp;
+
+    private boolean bulletIsDisable;
+    private boolean goUp;
+    FrameCounter frameCounter = new FrameCounter(10);
+
+    public Player(){
+        super();
+        this.renderer = new ImageRenderer(Utils.Loadimage("assets/image/player/1.png"));
+        this.velocity = new Vector2D();
+        instence = this;
+        vUp = new Vector2D(0,-4);
+    }
+
+    @Override
+    public void run(Vector2D parentPosition) {
+        super.run(parentPosition);
+        move();
+        makeBullet();
+    }
+
+    private void makeBullet() {
+        if (!bulletIsDisable){
+            if (inputManager.spacePressed){
+                //todo
+
+            }
+        }
+    }
+
+    private void move() {
+        velocity.set(0,0);
+        if (inputManager.spacePressed){
+            goUp = true;
+
+        }
+        if (inputManager.rightPressed){
+            velocity.addUp(2,0);
+        }
+        if (inputManager.leftPressed){
+            velocity.addUp(-2,0);
+        }
+        goUp();
+        position.addUp(velocity);
+    }
+
+    private void goUp() {
+        if (goUp){
+            if (frameCounter.run()){
+                frameCounter.reset();
+                vUp.addUp(0,0.5f);
+            }
+            velocity.addUp(vUp);
+            if (screenPosition.y >= 400){
+                goUp = false;
+                vUp.set(0,-4);
+            }
+        }
+    }
+
+    public void setInputManager(InputManager inputManager) {
+        this.inputManager = inputManager;
+    }
 }
