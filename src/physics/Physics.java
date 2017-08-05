25c3806b9269a0d96182a669c481dea0090afd70
cuonghx2709 <cuonghx2709@gmail.com>
2017-08-05 13:16:06 +0700

    fix physics

--- src/physics/Physics.java
+++ src/physics/Physics.java
@@ -2,6 +2,7 @@ package physics;
 
 import bases.Vector2D;
 import platforms.PlatformSprite;
+import players.Player;
 
 import java.util.Vector;
 
@@ -33,15 +34,14 @@ public class Physics {
 
         for(PhysicsBody body: bodies) {
             if (body.isActive() && body.getBoxCollider().collideWith(top, bottom, left, right)) {
-                if (body.getClass() == classz)
+                if (body.getClass().isInstance(classz))
                     return (T) body;
-           
             }
         }
 
         return null;
     }
-    public static <T extends PhysicsBody> T bodyInRectofPlatform(Vector2D position, float width, float height) {
+    public static <T extends PhysicsBody> T bodyInRectofsuper(Vector2D position, float width, float height, Class<T> classz) {
         float left = position.x - width / 2;
         float right = position.x + width / 2;
         float top = position.y - height / 2;
@@ -49,29 +49,13 @@ public class Physics {
 
         for(PhysicsBody body: bodies) {
             if (body.isActive() && body.getBoxCollider().collideWith(top, bottom, left, right)) {
-                if (body instanceof PlatformSprite)
+                if (body.getClass().getSuperclass() == classz)
                     return (T) body;
             }
         }
 
         return null;
     }
-    public static <T extends PhysicsBody> T bodyInRectofEnemy(Vector2D position, float width, float height) {
-        float left = position.x - width / 2;
-        float right = position.x + width / 2;
-        float top = position.y - height / 2;
-        float bottom = position.y + height / 2;
-
-        for(PhysicsBody body: bodies) {
-            if (body.isActive() && body.getBoxCollider().collideWith(top, bottom, left, right)) {
-                if (body instanceof PlatformSprite)
-                    return (T) body;
-            }
-        }
-
-        return null;
-    }
-
     public static<T extends PhysicsBody> T find(Class<T> classz){
         for (PhysicsBody body : bodies){
             if (body.isActive() && (body.getClass() == classz)){
