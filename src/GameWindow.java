71c62b43e4ef53ba525eca3103168f1beeff12a0
NHEM <danghai.hy,1996@gmail.com>
2017-08-12 04:14:12 +0700

    add introAudio

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -37,7 +37,7 @@ public class GameWindow extends JFrame implements Setting{
 
     public GameWindow() throws IOException {
         setUpgameWindow();
-        setAudio();
+        //setAudio();
         setupInputs();
         setUpStartupScene();
         this.setVisible(true);
